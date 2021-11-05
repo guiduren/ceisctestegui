c8bd605c562c2594126cbfaa2b4d0f4273c33334
guiduren <gustavo@ceisc.com.br>
2021-11-05 11:01:10 -0300

    Criação do idcurso em aulas

--- app/Aula.php
+++ app/Aula.php
@@ -4,17 +4,22 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Model;
+use App\Curso;
 
 class Aula extends Model{
 
     use Notifiable;
 
     protected $fillable = [
-        'nome', 'descricao', 'disponivel',
+        'nome', 'descricao', 'disponivel','curso_id',
     ];
 
     protected $casts = [
         'disponivel' => 'datetime',
     ];
 
+    public function curso()
+    {
+        return $this->hasOne('App\Curso','id','curso_id');
+    }
 }
